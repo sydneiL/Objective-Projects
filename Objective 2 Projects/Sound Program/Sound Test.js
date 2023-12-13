051597d7ff95c533996f02662937db49f19f2ca5
Sydnei Lang <90538664+sydneiL@users.noreply.github.com>
2023-12-13 08:57:19 -0600

    Add files via upload

---
+++ Objective 2 Projects/Sound Program/Sound Test.js
@@ -0,0 +1,16 @@
+//*Sound Test js file\\*
+
+function start()
+
+{      
+       document.getElementById("StartButton").disabled= true;
+       document.getElementById("StopButton").disabled= false;
+
+
+}
+function stop()
+{       
+document.getElementById("StartButton").disabled= false;
+document.getElementById("StopButton").disabled= true;
+
+}
